a686f8309f377565cb2cb678ec249861b764c5c4
rachelxie <rachelxie1985@gmail.com>
2017-03-06 11:04:42 +1300

    First commit

---
+++ MVCAgularJSExample/App/PerosnApp.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.title = "Person Table";
+    $scope.person = [{ "Name": "Andy", "Address": "1 Highway", "PhoneNumber": "12345" },
+                      { "Name": "Jackie", "Address": "2 Highway", "PhoneNumber": "54321" },
+                    { "Name": "Rachel", "Address": "3 Highway", "PhoneNumber": "22222" }];
+});
---
+++ MVCAgularJSExample/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAgularJSExample.Models
+{
+    public class PersonModel
+    {
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+    }
+}
